9e14c541e0d4892fd1c5e4d0461faa4ef9b357b6
Pankaj Kumar <87571351+pnkjkr21@users.noreply.github.com>
2026-09-01 00:12:27 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,19 @@
+# import sys
+# sys.setrecursionlimit(10**5)
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        self.ans = []
+        self.solve(nums, [], 0, target)
+        return self.ans
+    
+    def solve(self, nums, arr, ind, target):
+        if ind >= len(nums) or sum(arr) > target:
+            return
+        if sum(arr) == target:
+            self.ans.append(arr)
+            return
+        self.solve(nums, arr + [nums[ind]], ind, target)
+        self.solve(nums, arr, ind + 1, target)
+        
+
+        
